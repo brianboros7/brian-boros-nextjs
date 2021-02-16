dfacdc937c5e65975667d8374b7d754b61e8cf93
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-02-16 16:13:05 -0600

    using bootstrap containers, all sass is working, new variables and mixins files added, and homepage relatively completed

--- pages/index.js
+++ pages/index.js
@@ -5,38 +5,41 @@ import Header from '../components/app/header/Header'
 import {Container, Row, Col} from 'react-bootstrap'; 
 
 export default function Home() {
+
   return (
+
     <div>
       <Head>
         <title>Brian S.  Boros | Home</title>
       </Head>
-
+      <Header /> 
       <main> 
-        <Header />
-        <Container className="home-container">
-            <Row className="home-main"> 
-                <Col xs={7} lg={8} className="home-main-content">
-                    <h4 className=""> 
-                        let me help you build a
-				                growth strategy, driven 
-				                by data
-                    </h4>
-                    <h5 className="">
-                        Hi, I'm Brian Boros! I'm here to work with you
-				                to build, test, and learn how we can grow our accuracy
-				                with your go-to-market strategies.
-                    </h5>
-                </Col> 
-                <Col xs={5} lg={4} className="home-main-image"> 
-                  <Image
-                    src="/assets/images/brian-main.png"
-                    alt="Brian S. Boros Palying Sax"
-                    layout="fill"
-                  />
-                </Col> 
-            </Row> 
+        <Container className={styles["home-container"]}>
+          <Row className={styles["home-main"]}> 
+            <Col xs={8} lg={6} className={styles["home-main-content"]}>
+              <h2> 
+                let me help you build a
+				        growth strategy, driven 
+				        by data
+              </h2>
+              <h3>
+                Hi, I'm Brian Boros! I'm here to work with you
+				        to build, test, and learn how we can grow our accuracy
+				        with your go-to-market strategies.
+              </h3>
+            </Col> 
+            <Col xs={4} lg={6} className={styles["home-main-image"]}> 
+              <Image
+                src="/assets/images/brian-main.png"
+                className={["home-main-image"]}
+                alt="Brian S. Boros Palying Sax"
+                width={400}
+                height={500}
+              />
+            </Col> 
+          </Row> 
         </Container> 
       </main>
     </div>
-  )
-}
+    )
+  }
