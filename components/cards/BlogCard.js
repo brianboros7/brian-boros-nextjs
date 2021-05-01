f57672abeba8bbf1e6cf5d1abbf71e8958a13095
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-05-01 13:12:47 -0500

    data is comign through with apollo and the blog card, but the url and image will not work.

--- components/cards/BlogCard.js
+++ components/cards/BlogCard.js
@@ -1,32 +1,37 @@
 import Query from '../../hooks/useApollo'
-import Image from "next/image"
+import styles from '../../styles/Blog.module.scss'
+// import Image from "next/image"
 import Card  from "react-bootstrap/card"
 import { GET_ARTICLES } from '../../graphql/get-articles'
 import { gql } from "@apollo/client";
 import client from '../../lib/apolloClient'
 
 export default function BlogCard({ articles }) {
+    const url = "https://res.cloudinary.com/brian-boros-company/image/upload/"
 
     return(
         <div>
             <Query query={GET_ARTICLES}>
                 {({data: { articles }}) => { 
                     return(
-                        <div>
+                        <div className={styles["blog-card-container"]}>
                             {articles && articles.map((article) => (
                                 <div key={article.id}>
-                                    <Card>
-                                        <Image
+                                    <Card className={styles["blog-card"]}>
+                                        {/* <Image
                                             src={article.image[0].url}
                                             aslt="blog card image thumbnail"
                                             width={100}
                                             height={100}
                                             >
-                                        </Image>
+                                        </Image> */}
                                         <Card.Body>
                                             <Card.Title>
                                                     {article.title}
                                             </Card.Title>
+                                            <Card.Text>
+                                                {article.author} 
+                                            </Card.Text>
                                             <Card.Text>
                                                 {article.date} 
                                             </Card.Text>
